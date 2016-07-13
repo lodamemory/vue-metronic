5671168f1041df06e6ead6101f1b7dca950cdd84
wuyuyang128 <wuyuyang128@pingan.com.cn>
2016-07-13 18:42:47 +0800

    feat:select组件和date组件

--- src/components/mSelect.vue
+++ src/components/mSelect.vue
@@ -1,29 +1,28 @@
 <template>
-  <div class="select2-container form-control input-xlarge select2me select2-allowclear"
-    :class="{'select2-container-active': open, 'select2-dropdown-open': open}" v-el:container>
-    <a href="javascript:void(0)" class="select2-choice" @click="openSelect">
-      <span class="select2-chosen">{{selectName}}</span>
-      <abbr class="select2-search-choice-close" v-show="close"></abbr>
-        <span class="select2-arrow">
-            <b role="presentation"></b>
-        </span></a>
-    <label for="s2id_autogen2" class="select2-offscreen"></label>
-    <input class="select2-focusser select2-offscreen" type="text"  id="s2id_autogen2">
-  </div>
-  <!-- <div id="select2-drop-mask" class="select2-drop-mask" style="display:none" @click="openSelect" v-el:mask></div> -->
-  <div class="select2-drop select2-display-none select2-with-searchbox select2-drop-active s-show" 
-       v-show="open" id="select2-drop" v-el:drop>
-    <div class="select2-search"><label class="select2-offscreen"></label>
-      <input type="text" class="select2-input" id="s2id_autogen2_search" placeholder="" >
+    <label class="control-label">{{label}}</label>
+    <div class="select2-container form-control select2me select2-allowclear"
+    :class="[selectClass, {'select2-container-active': open, 'select2-dropdown-open': open}]" v-el:container>
+        <a href="javascript:void(0)" class="select2-choice" @click="openSelect">
+            <span class="select2-chosen">{{selectName}}</span>
+            <abbr class="select2-search-choice-close" v-show="close" @click.stop="deleteValue"></abbr>
+            <span class="select2-arrow">
+                <b role="presentation"></b>
+            </span>
+        </a>
     </div>
-    <ul class="select2-results" role="listbox" id="select2-results-2">
-      <li class="select2-results-dept-0 select2-result select2-result-selectable" v-for="d in data" @mouseover="mouseover($event)" @mouseout="mouseout($event)">
-        <div class="select2-result-label" @click="valueSelect(d)">
-            <span class="select2-match"></span>{{d.name}}
+    <div id="select2-drop-mask" class="select2-drop-mask" style="display:none" @click="openSelect" v-el:mask></div>
+    <div class="select2-drop select2-display-none select2-with-searchbox select2-drop-active s-show" v-show="open" v-el:drop>
+        <div class="select2-search"><label class="select2-offscreen"></label>
+            <input type="text" class="select2-input" placeholder="" v-model="filterStr">
         </div>
-      </li>
-    </ul>
-  </div>
+        <ul class="select2-results" role="listbox" v-el:ul>
+            <li class="select2-results-dept-0 select2-result select2-result-selectable" v-for="d in data" @mouseover="mouseover($event)" @mouseout="mouseout($event)" >
+            <div class="select2-result-label" @click="valueSelect(d)">
+                <span class="select2-match"></span>{{d.name}}
+            </div>
+            </li>
+        </ul>
+    </div>
 </template>
 <script>
     export default {
@@ -35,7 +34,37 @@
             'result': {
                 type: Object,
                 default: {}
+            },
+            'size': {
+                type: String,
+                default: 'medium'
+            },
+            'label': {
+                type: String,
+                default: ''
             }
+        },
+        data () {
+            return {
+                open: false,
+                active: false,
+                selectName: '',
+                close: false,
+                currentLi: '',
+                filterStr: '',
+                sData: [],
+                selectClass: {
+                    'input-xlarge': this.size === 'xlarge',
+                    'input-large': this.size === 'large',
+                    'input-medium': this.size === 'medium',
+                    'input-small': this.size === 'small',
+                    'input-xsmall': this.size === 'xsmall'
+                }
+            };
+        },
+        ready () {
+            this.sData = this.data;
+            window.console.log(this.sData);
         },
         methods: {
             openSelect () {
@@ -44,29 +73,51 @@
                     this.$els.mask.style.display = 'none';
                 } else {
                     this.open = true;
+                    this.filterStr = '';
                     this.$els.drop.style.width = this.$els.container.offsetWidth + 'px';
+                    this.$els.drop.style.top = this.$els.container.offsetTop + this.$els.container.offsetHeight + 'px';
+                    this.$els.drop.style.left = this.$els.container.offsetLeft + 'px';
                     this.$els.mask.style.display = 'block';
+                    for (var index in this.data) {
+                        if (this.data[index] === this.result) {
+                            this.$els.ul.children[index].classList.add('select2-highlighted');
+                            this.currentLi = index;
+                            break;
+                        }
+                    }
                 }
             },
             valueSelect (data) {
                 this.result = data;
                 this.selectName = data.name;
+                this.close = true;
                 this.open = false;
+                this.$els.mask.style.display = 'none';
+            },
+            deleteValue () {
+                this.close = false;
+                this.result = {};
+                this.selectName = '';
             },
             mouseover (event) {
+                if (this.currentLi !== '') this.$els.ul.children[this.currentLi].classList.remove('select2-highlighted');
                 event.currentTarget.classList.add('select2-highlighted');
             },
             mouseout (event) {
                 event.currentTarget.classList.remove('select2-highlighted');
             }
         },
-        data () {
-            return {
-                open: false,
-                active: false,
-                selectName: '',
-                close: false
-            };
+        watch: {
+            'filterStr': function (now, old) {
+                let temp = [];
+                for (var index in this.sData) {
+                    var key = this.sData[index].name;
+                    if (key.includes(now)) {
+                        temp.push(this.sData[index]);
+                    }
+                }
+                this.data = temp;
+            }
         }
     };
 </script>
--- src/example/example_select.vue
+++ src/example/example_select.vue
@@ -24,7 +24,14 @@
     <div class="row">
         <div class="col-md-12">
             <m-portlet :title="'Select'" :color-type="5">
-                <m-select :data="sData"></m-select>
+                <m-select :data="sData" :result.sync="sResult1" :size="'large'" :label="'医疗机构：'"></m-select>
+                <m-select :data="sData" :result.sync="sResult2"></m-select>
+                <m-select :data="sData" :result.sync="sResult1" :size="'small'" :label="'医疗机构：'"></m-select>
+                <m-select :data="sData" :result.sync="sResult2" :size="'xsmall'"></m-select>
+                <m-datepicker :format="'yyyy-MM-dd'" :value.sync="dateValue"></m-datepicker>
+                <div>{{sResult1.code}} {{sResult1.name}}</div>
+                <div>{{sResult2.code}} {{sResult2.name}}</div>
+                <div>{{dateValue}}</div>
             </m-portlet>
         </div>
         
@@ -35,15 +42,21 @@
     import mPortlet from '../components/mPortlet';
     import mButton from '../components/mButton';
     import mSelect from '../components/mSelect';
+    import mDatepicker from '../components/mDatepicker';
     export default {
-        components: { mPortlet, mSelect, mButton },
+        components: { mPortlet, mSelect, mButton, mDatepicker },
         data () {
             return {
                 aShow: false,
                 sData: [
                     {'code': 1, 'name': '我是哈哈哈'},
-                    {'code': 2, 'name': '我是呵呵呵'}
-                ]
+                    {'code': 2, 'name': '我是呵呵呵'},
+                    {'code': 3, 'name': '我是嘻嘻嘻'},
+                    {'code': 4, 'name': '我是哦哦哦'}
+                ],
+                sResult1: {},
+                sResult2: {},
+                dateValue: ''
             };
         }
     };
