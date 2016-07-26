432dbb1b493f54226adc0743f548fe594f6ae8ad
wuyuyang128 <wuyuyang128@pingan.com.cn>
2016-07-26 17:55:08 +0800

    feat: import修改

---
+++ src/components/componentsCss.js
@@ -0,0 +1,36 @@
+// BEGIN GLOBAL MANDATORY STYLES
+import mandatory1 from '../assets/global/plugins/font-awesome/css/font-awesome.min.css';
+import mandatory2 from '../assets/global/plugins/simple-line-icons/simple-line-icons.min.css';
+import mandatory3 from '../assets/global/plugins/bootstrap/css/bootstrap.min.css';
+import mandatory4 from '../assets/global/plugins/bootstrap-switch/css/bootstrap-switch.min.css';
+// END GLOBAL MANDATORY STYLES
+
+// BEGIN PAGE LEVEL PLUGINS
+// alert
+import alert1 from '../assets/global/plugins/bootstrap-toastr/toastr.min.css';
+// select
+import select1 from '../assets/global/plugins/select2/css/select2.min.css';
+import select2 from '../assets/global/plugins/select2/css/select2-bootstrap.min.css';
+// tree
+import tree1 from '../assets/global/plugins/jstree/dist/themes/default/style.min.css';
+// table
+import table1 from '../assets/global/plugins/datatables/datatables.min.css';
+import table2 from '../assets/global/plugins/datatables/plugins/bootstrap/datatables.bootstrap.css';
+// END PAGE LEVEL PLUGINS -->
+
+// BEGIN THEME GLOBAL STYLES
+import global1 from '../assets/global/css/components-md.min.css';
+import global2 from '../assets/global/css/plugins-md.min.css';
+import global3 from '../assets/global/css/font.css';
+import global4 from '../assets/animate.css';
+// END THEME GLOBAL STYLES
+
+// BEGIN THEME LAYOUT STYLES
+import layout1 from '../assets/layouts/layout4/css/layout.min.css';
+import layout2 from '../assets/layouts/layout4/css/themes/default.min.css';
+import layout3 from '../assets/layouts/layout4/css/custom.min.css';
+// END THEME LAYOUT STYLES
+
+export {
+    mandatory1, mandatory2, mandatory3, mandatory4, alert1, select1, select2, tree1, table1, table2, global1, global2, global3, global4, layout1, layout2, layout3
+};
--- src/main.js
+++ src/main.js
@@ -1,40 +1,7 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
 import routerMap from './router';
-
-// BEGIN GLOBAL MANDATORY STYLES
-import './assets/global/plugins/font-awesome/css/font-awesome.min.css';
-import './assets/global/plugins/simple-line-icons/simple-line-icons.min.css';
-import './assets/global/plugins/bootstrap/css/bootstrap.min.css';
-import './assets/global/plugins/bootstrap-switch/css/bootstrap-switch.min.css';
-// END GLOBAL MANDATORY STYLES
-
-// BEGIN PAGE LEVEL PLUGINS
-// alert
-import './assets/global/plugins/bootstrap-toastr/toastr.min.css';
-// select
-import './assets/global/plugins/select2/css/select2.min.css';
-import './assets/global/plugins/select2/css/select2-bootstrap.min.css';
-// tree
-import './assets/global/plugins/jstree/dist/themes/default/style.min.css';
-// table
-import './assets/global/plugins/datatables/datatables.min.css';
-import './assets/global/plugins/datatables/plugins/bootstrap/datatables.bootstrap.css';
-// END PAGE LEVEL PLUGINS -->
-
-// BEGIN THEME GLOBAL STYLES
-import './assets/global/css/components-md.min.css';
-import './assets/global/css/plugins-md.min.css';
-import './assets/global/css/font.css';
-// END THEME GLOBAL STYLES
-
-// BEGIN THEME LAYOUT STYLES
-import './assets/layouts/layout4/css/layout.min.css';
-import './assets/layouts/layout4/css/themes/default.min.css';
-import './assets/layouts/layout4/css/custom.min.css';
-// END THEME LAYOUT STYLES
-
-import './assets/animate.css';
+import './components/componentsCss';
 
 Vue.use(VueRouter);
 
